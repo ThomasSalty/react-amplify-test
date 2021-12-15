48cf69c20fd59f6c4d2b4cbced12c0a5309d6751
Tamas Soos <tamas_soos@epam.com>
2021-12-15 18:18:14 +0100

    Add auth to amplify.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "buildareactapplicati26409bcb": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
