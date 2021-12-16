90953438d8072586a350b81cd2b94e5a62712aef
Tamas Soos <tamas_soos@epam.com>
2021-12-16 14:56:48 +0100

    Add sign out button to header.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import './App.css';
-import Footer from './component/Footer';
-import Guarantee from './component/Guarantee';
-import Header from './component/Header';
-import History from './component/History';
-import People from './component/People';
-import Products from './component/Products';
+import React from "react";
+import "./App.css";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-class App extends React.Component {
+import Footer from "./component/Footer";
+import Guarantee from "./component/Guarantee";
+import Header from "./component/Header";
+import History from "./component/History";
+import People from "./component/People";
+import Products from "./component/Products";
 
-  render() {
+const App = ({ signOut, user }) => {
     return (
-      <div className="App">
-        <Header />
-        <History />
-        <Products />
-        <Guarantee />
-        <People />
-        <Footer />
-      </div>
+        <div className="App">
+            <Header signOut={signOut} email={user.attributes.email} />
+            <History />
+            <Products />
+            <Guarantee />
+            <People />
+            <Footer />
+        </div>
     );
-  }
-}
+};
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 Amplify.configure(awsconfig);
 
