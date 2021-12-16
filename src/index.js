a5d5f21a516497abf06bcb55c5c51269b7ad2ab9
Tamas Soos <tamas_soos@epam.com>
2021-12-16 17:35:43 +0100

    Translate login to Hungarian.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Amplify from "aws-amplify";
+import Amplify, { I18n } from "aws-amplify";
 import awsconfig from "./aws-exports";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { translations } from "@aws-amplify/ui";
+I18n.putVocabularies(translations);
+I18n.setLanguage("hu");
+
+I18n.putVocabularies({
+    hu: {
+        "Sign in": "Bejelentkezés",
+        "Sign In": "Bejelentkezés",
+        Password: "Jelszó",
+        "Forgot your password? ": "Elfelejtett jelszó",
+    },
+});
 
 Amplify.configure(awsconfig);
 
