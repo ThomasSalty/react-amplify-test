98cc7328871fed68940b6f8336818a7c5d7e29f3
Tamas Soos <tamas_soos@epam.com>
2021-12-16 17:52:48 +0100

    Remove unnecessary lines.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,11 @@ import Amplify, { I18n } from "aws-amplify";
 import awsconfig from "./aws-exports";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { translations } from "@aws-amplify/ui";
-I18n.putVocabularies(translations);
 I18n.setLanguage("hu");
 
 I18n.putVocabularies({
     hu: {
         "Sign in": "Bejelentkezés",
-        "Sign In": "Bejelentkezés",
         Password: "Jelszó",
         "Forgot your password? ": "Elfelejtett jelszó",
     },
